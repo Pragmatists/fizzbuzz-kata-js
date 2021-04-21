6bd0c18092efb972d7f5b83f01970636a4d17937
michal.lipski <kiwaczki@gmail.com>
2021-04-21 22:13:17 +0200

    solution with parameterized tests using jest

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,38 +1,50 @@
-const fizzbuzz = require('./fizzbuzz');
-const fizzbuzz_game = require('./fizzbuzz_game');
+const fizzbuzz = require('./fizzbuzz')
+const fizzbuzz_game = require('./fizzbuzz_game')
 
 describe('Fizz Buzz', function () {
 
-  [
+  it('gives answer 1 for 1', () => {
+    expect(fizzbuzz(1)).toEqual('1')
+  })
+
+  it('gives answer 2 for 2', () => {
+    expect(fizzbuzz(2)).toEqual('2')
+  })
+
+  it('gives answer Fizz for 3', () => {
+    expect(fizzbuzz(3)).toEqual('Fizz')
+  })
+
+  it('gives answer Buzz for 5', () => {
+    expect(fizzbuzz(5)).toEqual('Buzz')
+  })
+
+
+  it.each([
     [1, '1'],
     [2, '2'],
     [3, 'Fizz'],
     [6, 'Fizz'],
     [5, 'Buzz'],
-    [10, 'Buzz'],
     [15, 'FizzBuzz'],
-    [45, 'FizzBuzz']
-  ].forEach(function(params) {
-    var input = params[0], expected = params[1];
-
-    it('gives answer ' + expected + ' for ' + input, function () {
-      expect(fizzbuzz(input)).toEqual(expected);
-    });
-
+    [45, 'FizzBuzz']])
+  ('for value %i gives answer %s', (value, expected) => {
+    expect(fizzbuzz(value)).toBe(expected)
   });
 
-  describe('works for all the numbers', function() {
+
+  describe('works for all the numbers', function () {
     [
       [1, '1'],
       [2, '1,2'],
       [3, '1,2,Fizz']
-    ].forEach(function(params) {
-      var input = params[0], expected = params[1];
+    ].forEach(function (params) {
+      var input = params[0], expected = params[1]
 
       it('' + input, function () {
-        expect(fizzbuzz_game(input)).toEqual(expected);
-      });
+        expect(fizzbuzz_game(input)).toEqual(expected)
+      })
 
-    });
+    })
   })
-});
+})
